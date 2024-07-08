35552082dac82abc8526a6de8c4c934802f12eeb
Arthur Ticianeli <80481966+arthurticianeli@users.noreply.github.com>
2024-07-07 21:51:07 -0300

    Remoção de importações

--- src/main/java/com/arthurticianeli/petsfriends/PetsfriendsApplication.java
+++ src/main/java/com/arthurticianeli/petsfriends/PetsfriendsApplication.java
@@ -3,14 +3,13 @@ package com.arthurticianeli.petsfriends;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class PetsfriendsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PetsfriendsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PetsfriendsApplication.class, args);
+    }
 
 }
--- src/main/java/com/arthurticianeli/petsfriends/applications/pets/domain/dtos/PetRequestFilterDto.java
+++ src/main/java/com/arthurticianeli/petsfriends/applications/pets/domain/dtos/PetRequestFilterDto.java
@@ -1,9 +1,8 @@
 package com.arthurticianeli.petsfriends.applications.pets.domain.dtos;
 
-import com.arthurticianeli.petsfriends.applications.pets.domain.enums.PetStatus;
-
-public record PetRequestFilterDto (
+public record PetRequestFilterDto(
         String name,
         String category,
         String status
-){}
+) {
+}
